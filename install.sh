33a9c208ffd5e8fb8dcf3f6cc934576069682948
Joe Nelson <cred+github@begriffs.com>
2013-12-10 12:34:45 -0800

    Fix paths, add better messages

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+msg() { echo "--- $@" 1>&2; }
+detail() { echo "	$@" 1>&2; }
 
 for i in git cabal make vim; do
   command -v $i >/dev/null
   if [ $? -ne 0 ] ; then
-    echo "Installer requires ${i}. Please install $i and try again."
+    msg "Installer requires ${i}. Please install $i and try again."
     exit 1
   fi
 done
@@ -11,23 +14,26 @@ done
 endpath="$HOME/.haskell-vim-now"
 
 if [ ! -e $endpath/.git ]; then
-  echo "Cloning begriffs/haskell-vim-now"
+  msg "Cloning begriffs/haskell-vim-now"
   git clone https://github.com/begriffs/haskell-vim-now.git $endpath
 else
-  echo "Updating begriffs/haskell-vim-now"
+  msg "Existing installation detected"
+  msg "Updating from begriffs/haskell-vim-now"
   cd $endpath && git pull
 fi
 
 if [ -e ~/.vim/colors ]; then
-  echo "Preserving color scheme files"
+  msg "Preserving color scheme files"
   cp -R ~/.vim/colors $endpath/colors
 fi
 
-echo "Backing up current vim config"
 today=`date +%Y%m%d_%H%M%S`
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+msg "Backing up current vim config"
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today && detail "$i.$today"; done
 
-echo "Creating symlinks"
+msg "Creating symlinks"
+detail "~/.vimrc -> $endpath/.vimrc"
+detail "~/.vim   -> $endpath/.vim"
 ln -sf $endpath/.vimrc $HOME/.vimrc
 if [ ! -d $endpath/.vim/bundle ]; then
   mkdir -p $endpath/.vim/bundle
@@ -35,17 +41,15 @@ fi
 ln -sf $endpath/.vim $HOME/.vim
 
 if [ ! -e $HOME/.vim/bundle/vundle ]; then
-  echo "Installing Vundle"
+  msg "Installing Vundle"
   git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 fi
 
-echo "Installing plugins using Vundle"
+msg "Installing plugins using Vundle"
 system_shell=$SHELL
 export SHELL="/bin/sh"
-vim -u $endpath/.vimrc +BundleInstall! +BundleClean +qall
+vim -u $endpath/.vimrc +BundleInstall! +BundleClean! +qall
 export SHELL=$system_shell
 
-echo "Building C extension of vimproc.vim"
-cd $endpoint/bundle/vimproc.vim
-make
-cd -
+msg "Building vimproc.vim"
+make -C $endpath/.vim/bundle/vimproc.vim
