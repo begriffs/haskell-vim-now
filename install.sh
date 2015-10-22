d03994612e7262b79bd0bd7d1c35ceccb9340647
Joe Nelson <cred+github@begriffs.com>
2015-10-22 10:13:44 -0700

    Smoother stack setup

--- install.sh
+++ install.sh
@@ -91,13 +91,13 @@ vim -T dumb -E -u $endpath/.vimrc +PluginInstall! +PluginClean! +qall
 msg "Building vimproc.vim"
 make -C ~/.vim/bundle/vimproc.vim
 
-msg "Adding extra stack deps if needed"
-DEPS_REGEX='s/extra-deps: \[\]/extra-deps: [cabal-helper-0.6.1.0, pure-cdb-0.1.1]/'
-sed -i.bak "$DEPS_REGEX" ~/.stack/global/stack.yaml || sed -i.bak "$DEPS_REGEX" ~/.stack/global-project/stack.yaml
-rm -f ~/.stack/global/stack.yaml.bak ~/.stack/global-project/stack.yaml.bak
-
 msg "Setting up GHC if needed"
 stack setup
+
+msg "Adding extra stack deps if needed"
+DEPS_REGEX='s/extra-deps: \[\]/extra-deps: [cabal-helper-0.6.1.0, pure-cdb-0.1.1]/'
+sed -i.bak "$DEPS_REGEX" ~/.stack/global-project/stack.yaml || sed -i.bak "$DEPS_REGEX" ~/.stack/global/stack.yaml
+rm -f ~/.stack/global/stack.yaml.bak ~/.stack/global-project/stack.yaml.bak
 
 msg "Installing helper binaries"
 stack --resolver nightly install ghc-mod hasktags codex hscope pointfree pointful hoogle stylish-haskell
